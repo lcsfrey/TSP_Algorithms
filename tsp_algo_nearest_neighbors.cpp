11f55708c7778466b88d41ed94f71b9b03e33e54
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-08-17 22:35:40 -0700

    Significantly reduced memory usage by removing copy of vertices originally used in findPath method

--- tsp_algo_nearest_neighbors.cpp
+++ tsp_algo_nearest_neighbors.cpp
@@ -4,36 +4,37 @@
 void TSP_Algo_Nearest_Neighbors::findPath(){
     printf("Finding nearest neighbor route...\n");
     auto start = std::chrono::high_resolution_clock::now();
-    int size = m_vertices.size();
-    long int t_total_route_length = 0;
+    int size = m_vertices->size();
+    int t_total_route_length = 0;
 
     // copy of vertices
     // necessary to use copy to preserve integrity of original vertices' min heaps
-    vector<Vertex> t_vertices(m_graph->getVertices());
+    //vector<Vertex> m_vertices(m_graph->getVertices());
     // record of which vertices are in the route
-    vector<bool> inRoute(t_vertices.size(), false);
+    vector<bool> inRoute(m_vertices->size(), false);
 
     // record of current route
-    vector<int> t_simple_route(size);
+    vector<short> t_simple_route(size);
+
 
     // pointer to min heap of the current vertex
     std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>>* t_heap;
 
     // set heap to be the heap of the first vertex and pop off the top edge
-    t_heap = &(t_vertices.front().getEdgeHeap());
+    t_heap = &(m_vertices->front().getEdgeHeap());
     Edge current_edge = t_heap->top();
     t_total_route_length = current_edge.getWeight();
     t_heap->pop();
 
     // set heap to be the heap of
-    t_heap = &t_vertices[current_edge.getTo()].getEdgeHeap();
+    t_heap = &(m_vertices->at(current_edge.getTo()).getEdgeHeap());
 
     inRoute[current_edge.getFrom()] = true;
     inRoute[current_edge.getTo()] = true;
     t_simple_route[0] = current_edge.getFrom();
     t_simple_route[1] = current_edge.getTo();
 
-    int num_in_tour = 1;
+    short num_in_tour = 1;
     while(num_in_tour < size-1){
         current_edge = t_heap->top();
 
@@ -44,7 +45,7 @@ void TSP_Algo_Nearest_Neighbors::findPath(){
             current_edge = t_heap->top();
         }
         t_heap->pop();
-        t_heap = &(t_vertices[current_edge.getTo()].getEdgeHeap());
+        t_heap = &(m_vertices->at(current_edge.getTo()).getEdgeHeap());
 
         // add vertex of traversed edge to the route
         t_simple_route[++num_in_tour] = current_edge.getTo();
@@ -53,7 +54,7 @@ void TSP_Algo_Nearest_Neighbors::findPath(){
     }
 
     // edge connecting the end to the beginning
-    Edge last_edge(t_vertices[current_edge.getTo()], t_vertices[t_simple_route[0]]);
+    Edge last_edge(m_vertices->at(current_edge.getTo()), m_vertices->at(t_simple_route[0]));
     t_total_route_length += last_edge.getWeight();
 
     m_simple_route = t_simple_route;
@@ -65,25 +66,16 @@ void TSP_Algo_Nearest_Neighbors::findPath(){
     printf("Route length: %d\n----------------------------------------\n", m_route_length);
 }
 
-vector<int> TSP_Algo_Nearest_Neighbors::getRoute(){
-    return m_simple_route;
-}
-
-int TSP_Algo_Nearest_Neighbors::getRouteLength(){
-    return m_route_length;
-}
-
 void TSP_Algo_Nearest_Neighbors::twoOpt(chrono::high_resolution_clock::time_point time_overall_start){
-
-    std::chrono::duration<double> time_overall;
-
     printf("Finding 2-optimal route...\n");
     auto start = std::chrono::high_resolution_clock::now();
     auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_overall;
+    std::chrono::duration<double> elapsed;
 
     int old_length = this->m_route_length;
     int current_length = this->m_route_length;
-    vector<int> current_route(this->m_simple_route);
+    vector<short> current_route(this->m_simple_route);
 
     Edge *old_edge_1;
     Edge *old_edge_2;
@@ -95,35 +87,41 @@ void TSP_Algo_Nearest_Neighbors::twoOpt(chrono::high_resolution_clock::time_poin
     int new_edge_2_weight;
 
     int size = m_simple_route.size();
-    int iterations = 0;
+    // indices of vertices in the route to reverse
     int min_i, min_k;
     int min_change = 0;
-
+    int change = 0;
+    int iterations = 0;
 
     do{
+        // initialize loop variables
         min_change = 0;
         min_i = -1;
         old_length = current_length;
         iterations++;
+
+        // loop through every pair of edges in the current path
         for(int i = 0; i < size-2; i++){
             for(int k = i+2; k < size; k++){
 
-                old_edge_1 = &(m_vertices[current_route[i]].getEdge(current_route[i+1]));
-                new_edge_1 = &(m_vertices[current_route[i]].getEdge(current_route[k]));
+                old_edge_1 = &(m_vertices->at(current_route[i]).getEdge(current_route[i+1]));
+                new_edge_1 = &(m_vertices->at(current_route[i]).getEdge(current_route[k]));
                 if(k < size-1){
-                    old_edge_2 = &(m_vertices[current_route[k]].getEdge(current_route[k+1]));
-                    new_edge_2 = &(m_vertices[current_route[i+1]].getEdge(current_route[k+1]));
+                    old_edge_2 = &(m_vertices->at(current_route[k]).getEdge(current_route[k+1]));
+                    new_edge_2 = &(m_vertices->at(current_route[i+1]).getEdge(current_route[k+1]));
                 }
                 else{
-                    old_edge_2 = &(m_vertices[current_route[k]].getEdge(current_route[0]));
-                    new_edge_2 = &(m_vertices[current_route[i+1]].getEdge(current_route[0]));
+                    old_edge_2 = &(m_vertices->at(current_route[k]).getEdge(current_route[0]));
+                    new_edge_2 = &(m_vertices->at(current_route[i+1]).getEdge(current_route[0]));
                 }
+
                 old_edge_1_weight = old_edge_1->getWeight();
                 old_edge_2_weight= old_edge_2->getWeight();
                 new_edge_1_weight= new_edge_1->getWeight();
                 new_edge_2_weight= new_edge_2->getWeight();
-                int change = new_edge_1_weight + new_edge_2_weight - old_edge_1_weight - old_edge_2_weight;
 
+                // check if the weight of the new edges is less than the weight of the old edges
+                change = new_edge_1_weight + new_edge_2_weight - old_edge_1_weight - old_edge_2_weight;
                 if(min_change > change){
                     min_change = change;
                     min_i = i+1;
@@ -131,41 +129,45 @@ void TSP_Algo_Nearest_Neighbors::twoOpt(chrono::high_resolution_clock::time_poin
                 }
             }
         }
-
+        // reverse order of route between two indices
+        // logically
         if(min_i != -1){
             current_length += min_change;
-            vector<int>::iterator t_end = current_route.begin() + min_k;
+            vector<short>::iterator t_end = current_route.begin() + min_k;
             std::reverse(current_route.begin() + min_i, t_end);
         }
 
+        // report time and length of every 25th iteration
         finish = std::chrono::high_resolution_clock::now();
         time_overall = finish - time_overall_start;
         if(iterations % 25 == 0){
-            std::chrono::duration<double> elapsed = finish - start;
+            elapsed = finish - start;
             printf("%d iterations took: %f seconds\n",iterations, elapsed.count());
             printf("Current route length: %d\n----------------------------------------\n", current_length);
         }
-    }while(iterations <= 1000 && old_length != current_length && time_overall.count() < 175);
+    // repeat for 1000 iterations, until the length hasn't changed between iterations, or until 170 seconds have passed
+    }while(iterations <= 1500 && old_length != current_length && time_overall.count() < 170);
+
 
     this->m_simple_route = current_route;
     this->m_route_length = calcPathLength(m_simple_route);
 
     finish = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = finish - start;
+    elapsed = finish - start;
     printf("2-optimal took: %f seconds\n", elapsed.count());
     printf("Final length: %d\n", m_route_length);
     printf("Total time taken: %f seconds\n----------------------------------------\n", time_overall.count());
 }
 
-int TSP_Algo_Nearest_Neighbors::calcPathLength(vector<int>& t_route){
+int TSP_Algo_Nearest_Neighbors::calcPathLength(vector<short>& t_route){
     int t_total_length = 0;
     int size = t_route.size() - 1;
     Edge* current_edge;
     for(int i = 0; i < size; i++){
-       current_edge = &(m_vertices[t_route[i]].getEdge(t_route[i+1]));
+       current_edge = &(m_vertices->at(t_route[i]).getEdge(t_route[i+1]));
        t_total_length += current_edge->getWeight();
     }
-    current_edge = &(m_vertices[t_route[size]].getEdge(t_route[0]));
+    current_edge = &(m_vertices->at(t_route[size]).getEdge(t_route[0]));
     t_total_length += current_edge->getWeight();
     return t_total_length;
 }
@@ -181,10 +183,3 @@ void TSP_Algo_Nearest_Neighbors::writeToFile(std::string file_name){
     }
     file.close();
 }
-
-//std::vector<int> TSP_Algo_Nearest_Neighbors::bruteForce(vector<<vector<int>*> stacks, int size){
-//    stacks
-//    for(int i = 0; i < size; i++){
-
-//    }
-//}
--- tsp_algo_nearest_neighbors.h
+++ tsp_algo_nearest_neighbors.h
@@ -10,22 +10,24 @@ class TSP_Algo_Nearest_Neighbors
 {
 public:
     TSP_Algo_Nearest_Neighbors(Graph* t_graph) : m_graph(t_graph){
-        m_vertices = m_graph->getVertices();
+        m_vertices = &(m_graph->getVertices());
     }
-
     void findPath();
     void twoOpt(chrono::system_clock::time_point time_overall_start = std::chrono::high_resolution_clock::now());
-    std::vector<int> getRoute();
-    int getRouteLength();
+
+    inline vector<short> getRoute(){ return m_simple_route; }
+
+    inline int getRouteLength(){ return m_route_length; }
     void writeToFile(std::string file_name);
 
 
+    void optimalDP();
 private:
-    int calcPathLength(vector<int>& t_route);
+    int calcPathLength(vector<short>& t_route);
     int m_route_length = 0;
-    vector<Vertex> m_vertices;
+    vector<Vertex>* m_vertices;
     vector<Edge> m_route;
-    vector<int> m_simple_route;
+    vector<short> m_simple_route;
     Graph *m_graph;
 
 };
