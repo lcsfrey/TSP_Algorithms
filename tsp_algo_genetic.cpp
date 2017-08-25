fa0883ef4fecb1cb303aadc8f2b86c635ca5863c
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-08-25 14:52:24 -0700

    Merge branch 'master' of https://github.com/lcsfrey/TSP_Algorithm

--- tsp_algo_genetic.cpp
+++ tsp_algo_genetic.cpp
@@ -1,27 +1,27 @@
 #include "tsp_algo_genetic.h"
+#include "tsp_algo_nearest_neighbors.h"
 #include <iostream>
 
 int TSP_Algo_Genetic::getCurrentFitness() const{
-    return this->m_fitness;
+    return this->m_best_fitness;
 }
 
-vector<Edge> TSP_Algo_Genetic::getRoute(){
-    return this->m_route;
+std::vector<int> TSP_Algo_Genetic::getRoute() const {
+    return this->m_best_route;
 }
 
 void TSP_Algo_Genetic::tick(){
     calcFitness();
+    m_last_fitness = m_best_fitness;
+    //m_fitness = TSP_Algos::calcPathLength(this->m_graph, m_best_route);
 }
 
-void TSP_Algo_Genetic::calcFitness(){
-    m_last_fitness = m_fitness;
-    int t_total_fitness = 0, t_current_fitness = 0;
-    int size = this->m_route.size();
-    for(int i = 0; i < size; i++){
-        std::vector<Edge> edges = m_vertices->at(i).getEdges();
-        t_current_fitness = edges.at(m_min_spanning_tree[i]).getWeight();
-        t_total_fitness += t_current_fitness;
+void TSP_Algo_Genetic::calcFitness() {
+    int t_total_fitness = 0;
+    int size = this->m_best_route.size();
+    for(int i = 0; i < size - 1; i++){
+        t_total_fitness += this->m_graph->getEdgeWeight(m_best_route[i], m_best_route[i+1]);
     }
-    m_fitness = t_total_fitness;
-
+     t_total_fitness += this->m_graph->getEdgeWeight(m_best_route[size-1], m_best_route[0]);
+    m_best_fitness = t_total_fitness;
 }
--- tsp_algo_genetic.h
+++ tsp_algo_genetic.h
@@ -1,28 +1,75 @@
 #ifndef TSP_Algo_Genetic_H
 #define TSP_Algo_Genetic_H
 #include "graph.h"
+#include <algorithm>
+#include <queue>
+
+namespace TSP_Algos{
+
+}
 
 class TSP_Algo_Genetic
 {
 public:
-    TSP_Algo_Genetic(std::string &file_name) : m_graph(file_name){
-        m_min_spanning_tree = m_graph.primMST();
-        m_vertices = new vector<Vertex>(m_graph.getVertices());
+    TSP_Algo_Genetic(Graph* t_graph, int &population_size, int max_generations=1000, int max_species=500) : m_graph(t_graph){
+        m_vertices = new std::vector<Vertex>(m_graph->getVertices());
+
+        // creates all populations and stores them in min heap
+        for(int i = 0; i < population_size; i++) {
+            std::vector<int> t_route(m_vertices->size());
+
+            std::iota(t_route.begin(), t_route.end(), 1);
+
+            std::vector<std::vector<int>::iterator> t_vert_iterators(t_route.size());
+            std::iota(t_vert_iterators.begin(), t_vert_iterators.end(), t_route.begin());
+
+            std::shuffle(t_vert_iterators.begin(), t_vert_iterators.end(), std::mt19937{std::random_device{}()});
+
+            //m_populations.push_back(Population(t_route));
+            m_pop_heap.push(Population(t_graph, t_route));
+
+            std::cout << "Contents of the list: ";
+            for(auto n: t_route) std::cout << n << ' ';
+            std::cout << '\n';
+
+            std::cout << "Contents of the list, shuffled: ";
+            for(auto i: t_vert_iterators) std::cout << *i << ' ';
+            std::cout << '\n';
+        }
+
     }
 
     int getCurrentFitness() const;
+    struct Population {
+            Population(Graph* t_graph, const std::vector<int> &t_route) {
+                m_route = t_route;
+                m_population_fitness = t_graph->calcPathLength(t_graph, t_route);
+            }
+            void mutate(int t_probability);
+            Population reproduceWith(const Population& other_population) {
+                std::vector<int> t_child;
+                //std::vector<int> t_child;
+            }
+            bool operator> (const Population& other_pop) const {
+                return m_population_fitness > other_pop.m_population_fitness;
+            }
+            int m_population_fitness;
+            std::vector<int> m_route;
+    };
 
-    vector<Edge> getRoute();
+    std::vector<int> getRoute() const;
     void tick();
 
-private:
+    friend class Population;
+protected:
     void calcFitness();
-    int m_fitness = INT32_MAX;
-    int m_last_fitness;
-    vector<Vertex>* m_vertices;
-    vector<Edge> m_route;
-    vector<int> m_min_spanning_tree;
-    Graph m_graph;
+    int m_best_fitness = INT32_MAX;
+    int m_last_fitness = 0;
+    const std::vector<Vertex>* m_vertices;
+    std::vector<int> m_best_route;
+    std::priority_queue<Population, std::vector<Population>, std::greater<Population>> m_pop_heap;
+    std::vector<Population> m_populations;
+    Graph* m_graph;
 
 };
 
