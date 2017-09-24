54deda662c883655211a734fcecdd2d605e863a4
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-09-24 14:47:10 -0700

    moved definitions out of header and into cpp file

--- tsp_algo_genetic.cpp
+++ tsp_algo_genetic.cpp
@@ -4,6 +4,28 @@
 
 namespace TSP_Algos {
 
+TSP_Algo_Genetic::TSP_Algo_Genetic(Graph *t_graph, int &Chromosome_size) : m_graph(t_graph) {
+    srand(time(0));
+    m_vertices = new std::vector<Vertex>(m_graph->getVertices());
+    m_best_fitness = INT32_MAX;
+    m_last_fitness = 0;
+    m_mutation_probability = .25;
+    std::priority_queue<Chromosome, std::vector<Chromosome>, std::greater<Chromosome>> temp_heap;
+
+    // creates random Chromosomes and stores the best Chromosomes in min heap
+    std::vector<int> t_route(m_vertices->size());
+    std::iota(t_route.begin(), t_route.end(), 0);
+    for(int i = 0; i < 30000; i++) {
+        std::shuffle(t_route.begin(), t_route.end(), std::mt19937{std::random_device{}()});
+        temp_heap.push(Chromosome(t_graph, t_route));
+    }
+    for (int i = 0; i < Chromosome_size; i++) {
+        Chromosome candidate = temp_heap.top();
+        m_chromosome_heap.push(candidate);
+        temp_heap.pop();
+    }
+}
+
 int TSP_Algo_Genetic::getCurrentFitness() const{
     return this->m_best_fitness;
 }
@@ -47,7 +69,7 @@ void TSP_Algo_Genetic::tick(){
     m_chromosome_heap = temp_chromosome_heap;
     int current_fitness = m_chromosome_heap.top().m_route_fitness;
     if (m_best_fitness == current_fitness) {
-        if (m_mutation_probability < .6) {
+        if (m_mutation_probability < .75) {
             m_mutation_probability += .005;
         }
     } else {
@@ -55,10 +77,47 @@ void TSP_Algo_Genetic::tick(){
             m_last_fitness = m_best_fitness;
             m_best_fitness = current_fitness;
         }
-        if (m_mutation_probability >= .35) {
-            m_mutation_probability -= .1;
+        if (m_mutation_probability >= .30) {
+            m_mutation_probability -= .05;
         }
     }
+}
+
+TSP_Algo_Genetic::Chromosome TSP_Algo_Genetic::reproduce(const TSP_Algo_Genetic::Chromosome &parent_1, const TSP_Algo_Genetic::Chromosome &parent_2) const {
+    int size = m_vertices->size();
+    int size_from_parent_1 = rand() % size;
+    int offset_from_parent_1 = rand() % (size - size_from_parent_1);
+    std::vector<int> t_child;
+    std::vector<bool> in_path(size, false);
+    for (int i = offset_from_parent_1; i < size_from_parent_1 + offset_from_parent_1; i++) {
+        t_child.push_back(parent_1.m_route[i]);
+        in_path[parent_1.m_route[i]] = true;
+    }
+    for (int i = 0; i < size; i++) {
+        if (!in_path[parent_2.m_route[i]]) {
+            t_child.push_back(parent_2.m_route[i]);
+        }
+    }
+    Chromosome child_chromosome(m_graph, t_child);
+    child_chromosome.mutate(m_mutation_probability);
+    child_chromosome.m_route_fitness = m_graph->calcPathLength(child_chromosome.m_route);
+    return child_chromosome;
+}
+
+TSP_Algo_Genetic::Chromosome::Chromosome(const Graph *t_graph, const std::vector<int> &t_route) : m_route(t_route) {
+    m_route_fitness = t_graph->calcPathLength(m_route);
+}
+
+void TSP_Algo_Genetic::Chromosome::mutate(double t_probability) {
+    if((double) rand() / RAND_MAX <= t_probability) {
+        int size = m_route.size();
+        std::swap(m_route[rand() % size], m_route[rand() % size]);
+        mutate(t_probability - .005);
+    }
+}
+
+bool TSP_Algo_Genetic::Chromosome::operator >(const Chromosome& other_chromosome) const{
+    return m_route_fitness > other_chromosome.m_route_fitness;
 }
 
 }
--- tsp_algo_genetic.h
+++ tsp_algo_genetic.h
@@ -1,9 +1,8 @@
-#ifndef TSP_Algo_Genetic_H
-#define TSP_Algo_Genetic_H
+#ifndef TSP_Algo_Genetic_H_
+#define TSP_Algo_Genetic_H_
 #include "graph.h"
 #include <algorithm>
 #include <queue>
-#include <unordered_map>
 #include <ctime>
 
 namespace TSP_Algos{
@@ -11,70 +10,21 @@ namespace TSP_Algos{
 class TSP_Algo_Genetic
 {
 public:
-    TSP_Algo_Genetic(Graph* t_graph, int &Chromosome_size) : m_graph(t_graph) {
-        srand(time(0));
-        m_vertices = new std::vector<Vertex>(m_graph->getVertices());
-        m_best_fitness = INT32_MAX;
-        m_last_fitness = 0;
-        m_mutation_probability = .25;
-        std::priority_queue<Chromosome, std::vector<Chromosome>, std::greater<Chromosome>> temp_heap;
-
-        // creates random Chromosomes and stores the best Chromosomes in min heap
-        std::vector<int> t_route(m_vertices->size());
-        std::iota(t_route.begin(), t_route.end(), 0);
-        for(int i = 0; i < 50000; i++) {
-            std::shuffle(t_route.begin(), t_route.end(), std::mt19937{std::random_device{}()});
-            temp_heap.push(Chromosome(t_graph, t_route));
-        }
-        for (int i = 0; i < Chromosome_size; i++) {
-            Chromosome candidate = temp_heap.top();
-            m_chromosome_heap.push(candidate);
-            temp_heap.pop();
-        }
-    }
-
+    TSP_Algo_Genetic(Graph* t_graph, int &Chromosome_size);
     int getCurrentFitness() const;
     double getMutationProbability() const;
     std::vector<int> getRoute() const;
     void tick();
 
     struct Chromosome {
-            Chromosome(const Graph* t_graph, const std::vector<int> &t_route) : m_route(t_route) {
-                m_route_fitness = t_graph->calcPathLength(m_route);
-            }
-            void mutate(double t_probability) {
-                if((double) rand() / RAND_MAX <= t_probability) {
-                    int size = m_route.size();
-                    std::swap(m_route[rand() % size], m_route[rand() % size]);
-                    mutate(t_probability - .01);
-                }
-            }
-            bool operator> (const Chromosome& other_chromosome) const {
-                return m_route_fitness > other_chromosome.m_route_fitness;
-            }
             int m_route_fitness;
             std::vector<int> m_route;
+
+            Chromosome(const Graph* t_graph, const std::vector<int> &t_route);
+            void mutate(double t_probability);
+            bool operator> (const Chromosome& other_chromosome) const;
     };
-    Chromosome reproduce(const Chromosome& parent_1, const Chromosome& parent_2) {
-        int size = m_vertices->size();
-        int size_from_parent_1 = rand() % size;
-        int offset_from_parent_1 = rand() % (size - size_from_parent_1);
-        std::vector<int> t_child;
-        std::vector<bool> in_path(size, false);
-        for (int i = offset_from_parent_1; i < size_from_parent_1 + offset_from_parent_1; i++) {
-            t_child.push_back(parent_1.m_route[i]);
-            in_path[parent_1.m_route[i]] = true;
-        }
-        for (int i = 0; i < size; i++) {
-            if (!in_path[parent_2.m_route[i]]) {
-                t_child.push_back(parent_2.m_route[i]);
-            }
-        }
-        Chromosome child_chromosome(m_graph, t_child);
-        child_chromosome.mutate(m_mutation_probability);
-        child_chromosome.m_route_fitness = m_graph->calcPathLength(child_chromosome.m_route);
-        return child_chromosome;
-    }
+    Chromosome reproduce(const Chromosome& parent_1, const Chromosome& parent_2) const;
 
  protected:
     int m_best_fitness;
@@ -83,9 +33,8 @@ public:
     const std::vector<Vertex>* m_vertices;
     std::priority_queue<Chromosome, std::vector<Chromosome>, std::greater<Chromosome>> m_chromosome_heap;
     Graph* m_graph;
-
 };
 
 }
 
-#endif // TSP_algo_genetic_H
+#endif  // TSP_algo_genetic_H_
