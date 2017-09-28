3e9d978c13250ec3b61daf6093a7da465c8177b0
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-09-27 23:44:16 -0700

    Added run function, typedef for Chromosome heap, and documentation
    run(num_generations) function calls tick function num_generations times,
    
    typedef ChromosomeHeap for Chromosome priority_queue removes uneeded line length while improving readability

--- tsp_algo_genetic.h
+++ tsp_algo_genetic.h
@@ -5,36 +5,48 @@
 #include <queue>
 #include <ctime>
 
-namespace TSP_Algos{
+namespace TSP_Algos {
 
 class TSP_Algo_Genetic
 {
 public:
-    TSP_Algo_Genetic(Graph* t_graph, int &Chromosome_size);
+    struct Chromosome {
+        int m_route_fitness;
+        std::vector<int> m_route;
+
+        Chromosome(const Graph* t_graph, const std::vector<int> &t_route);
+        void mutate(double t_probability);
+        bool operator> (const Chromosome& other_chromosome) const;
+    };
+    // Min heap used to store Chromosomes where the top of the heap is the
+    // Chromosome that is storing the best route and smallest route length.
+    typedef std::priority_queue<Chromosome,
+                                std::vector<Chromosome>,
+                                std::greater<Chromosome>> ChromosomeHeap;
+    Chromosome reproduce(const Chromosome& parent_1, const Chromosome& parent_2) const;
+
+    //
+    TSP_Algo_Genetic(const Graph *t_graph, int &Chromosome_size);
+    // Uses current generation contained within m_chromosome_heap to produce
+    // a new generation, replacing the old generation with the new one.
+    void tick();
+    // calls the tick method a number of times equal to num_generations
+    void run(const int &num_generations=5000);
+
     int getCurrentFitness() const;
     double getMutationProbability() const;
+    // returns the current best route
     std::vector<int> getRoute() const;
-    void tick();
-
-    struct Chromosome {
-            int m_route_fitness;
-            std::vector<int> m_route;
-
-            Chromosome(const Graph* t_graph, const std::vector<int> &t_route);
-            void mutate(double t_probability);
-            bool operator> (const Chromosome& other_chromosome) const;
-    };
-    Chromosome reproduce(const Chromosome& parent_1, const Chromosome& parent_2) const;
 
  protected:
     int m_best_fitness;
     int m_last_fitness;
     double m_mutation_probability;
     const std::vector<Vertex>* m_vertices;
-    std::priority_queue<Chromosome, std::vector<Chromosome>, std::greater<Chromosome>> m_chromosome_heap;
-    Graph* m_graph;
+    ChromosomeHeap m_chromosome_heap;
+    const Graph* m_graph;
 };
 
-}
+}  // namespace TSP_Algos
 
 #endif  // TSP_algo_genetic_H_
