b8c19d696555d027caed679975c1fa5a575a23b5
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-10-02 23:34:51 -0700

    Added testMenu and testGenetic methods to main.cpp

--- main.cpp
+++ main.cpp
@@ -31,27 +31,112 @@
 #include "graph.h"
 #include "tsp_algo_nearest_neighbors.h"
 #include "tsp_algo_genetic.h"
-#include "graph_path_trie.h"
+//#include "graph_path_trie.h"
 
+using namespace std;
+int testMenu(int argc, char *argv[]);
+void testGenetic(Graph *t_graph);
 int showWindow();
 void printLicense();
 int displayGUI(int argc, char *argv[]);
+
+
 int main(int argc, char *argv[]) {
-    Graph* my_graph = new Graph("test-input-1.txt");
-    int population_size = 1000;
-
-
-    //Graph_path_trie my_path(my_graph, my_graph->getNumVertices());
-
     printLicense();
-    int choice = -1;
+    //testMenu(argc, argv);
+    Graph* my_graph = new Graph("tsp_test_cases/test-input-1.txt");
+    //Graph_path_trie my_path(my_graph, my_graph->getNumVertices());
+//    TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn1(my_graph);
+    //algo_nn1.findPath();
+    //algo_nn1.twoOpt();
+//    TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn2(my_graph);
+//    algo_nn2.findPath();
+//    algo_nn2.threadedTwoOpt();
 
-    TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn(my_graph);
-    //TSP_Algos::TSP_Algo_Genetic algo_g(my_graph, population_size);
-
-    return displayGUI(argc, argv);
+//    int population_size = 500;
+//    TSP_Algos::TSP_Algo_Genetic algo_g(my_graph, population_size);
+//    algo_g.run(10000);
+    TSP_Algos::TSP_Algo_Genetic_Threaded algo_tg(my_graph);
+    algo_tg.run();
+    return 0;
 }
 
+int testMenu(int argc, char *argv[]) {
+    Graph* my_graph = new Graph("tsp_test_cases/test-input-2.txt");
+    TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn(my_graph);
+    int choice = -1;
+    while (choice != 0) {
+        cout << "Select method:\n"
+             << "0 - Exit\n"
+             << "1 - Nearest Neighbor\n"
+             << "2 - Two Optimal\n"
+             << "3 - Threaded Two Optimal\n"
+             << "4 - Genetic\n"
+             << "5 - Launch GUI\n"
+             << "\n";
+        cin >> choice;
+        cout << endl;
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                algo_nn.findPath();
+                break;
+            case 2:
+                algo_nn.findPath();
+                algo_nn.twoOpt();
+                break;
+            case 3:
+                algo_nn.findPath();
+                algo_nn.threadedTwoOpt();
+                break;
+            case 4:
+                testGenetic(my_graph);
+                break;
+            case 5:
+                return displayGUI(argc, argv);
+                break;
+            break;
+            default:
+                cout << "Invalid choice!\n";
+                break;
+        }
+    }
+    return 0;
+}
+
+void testGenetic(Graph* t_graph) {
+    int choice = -1;
+    int num_runs = 5000;
+    int pop_size = 1000;
+    TSP_Algos::TSP_Algo_Genetic algo_g(t_graph);
+    do {
+        cout << "Select options:\n"
+             << "0 - Exit\n"
+             << "1 - Run algorithm\n"
+             << "2 - Next generation\n"
+             << "3 - Change population size\n"
+             << "4 - Change number of generations\n";
+        cin >> choice;
+        switch(choice) {
+            case 0:
+                break;
+            case 1:
+                algo_g.run(num_runs);
+            case 2:
+                algo_g.tick();
+            case 3:
+                cout << "Enter size:\n";
+                cin >> pop_size;
+                algo_g.changePopulationSize(pop_size);
+            case 4:
+                cout << "Enter number of gens:\n";
+                cin >> num_runs;
+            default:
+                cout << "Invalid choice!\n";
+            }
+    } while (choice != -1);
+}
 
 int displayGUI(int argc, char *argv[]) {
     QApplication a(argc, argv);
