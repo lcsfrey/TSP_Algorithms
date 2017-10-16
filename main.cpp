de0b5eadaaf03b55c9f4b83d1d02929f4c3ab631
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-10-16 13:01:00 -0700

    Added threaded genetic algorithm tests
    Also added stat display for genetic algorithms

--- main.cpp
+++ main.cpp
@@ -24,18 +24,19 @@
 **                                                                                 **
 ************************************************************************************/
 
-
-#include "mainwindow.h"
 #include <QApplication>
+#include <iostream>
 
 #include "graph.h"
 #include "tsp_algo_nearest_neighbors.h"
 #include "tsp_algo_genetic.h"
-//#include "graph_path_trie.h"
+#include "graph_path_trie.h"
+#include "mainwindow.h"
 
 using namespace std;
+
 int testMenu(int argc, char *argv[]);
-void testGenetic(Graph *t_graph);
+void testGenetic(Graph *t_graph, bool is_threaded = false);
 int showWindow();
 void printLicense();
 int displayGUI(int argc, char *argv[]);
@@ -43,36 +44,24 @@ int displayGUI(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
     printLicense();
-    //testMenu(argc, argv);
-    Graph* my_graph = new Graph("tsp_test_cases/test-input-1.txt");
-    //Graph_path_trie my_path(my_graph, my_graph->getNumVertices());
-//    TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn1(my_graph);
-    //algo_nn1.findPath();
-    //algo_nn1.twoOpt();
-//    TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn2(my_graph);
-//    algo_nn2.findPath();
-//    algo_nn2.threadedTwoOpt();
-
-//    int population_size = 500;
-//    TSP_Algos::TSP_Algo_Genetic algo_g(my_graph, population_size);
-//    algo_g.run(10000);
-    TSP_Algos::TSP_Algo_Genetic_Threaded algo_tg(my_graph);
-    algo_tg.run();
+    testMenu(argc, argv);
     return 0;
 }
 
 int testMenu(int argc, char *argv[]) {
-    Graph* my_graph = new Graph("tsp_test_cases/test-input-2.txt");
+    Graph* my_graph = new Graph("tsp_test_cases/test-input-6.txt");
     TSP_Algos::TSP_Algo_Nearest_Neighbors algo_nn(my_graph);
     int choice = -1;
     while (choice != 0) {
-        cout << "Select method:\n"
+        cout << "--------------------------------------------------------------------------------\n"
+             << "Select method:\n"
              << "0 - Exit\n"
              << "1 - Nearest Neighbor\n"
              << "2 - Two Optimal\n"
              << "3 - Threaded Two Optimal\n"
              << "4 - Genetic\n"
-             << "5 - Launch GUI\n"
+             << "5 - Threaded Genetic\n"
+             << "6 - Launch GUI\n"
              << "\n";
         cin >> choice;
         cout << endl;
@@ -94,9 +83,11 @@ int testMenu(int argc, char *argv[]) {
                 testGenetic(my_graph);
                 break;
             case 5:
+                testGenetic(my_graph, true);
+                break;
+            case 6:
                 return displayGUI(argc, argv);
                 break;
-            break;
             default:
                 cout << "Invalid choice!\n";
                 break;
@@ -105,15 +96,36 @@ int testMenu(int argc, char *argv[]) {
     return 0;
 }
 
-void testGenetic(Graph* t_graph) {
+void testGenetic(Graph* t_graph, bool is_threaded) {
     int choice = -1;
-    int num_runs = 5000;
+    int num_gens = 100;
+    int current_gen = 0;
     int pop_size = 1000;
-    TSP_Algos::TSP_Algo_Genetic algo_g(t_graph);
+
+    TSP_Algos::TSP_Algo_Genetic* algo_g = nullptr;
+    TSP_Algos::TSP_Algo_Genetic_Threaded* algo_tg = nullptr;
+    if (is_threaded) {
+        algo_tg = new TSP_Algos::TSP_Algo_Genetic_Threaded(t_graph);
+    } else {
+        algo_g = new TSP_Algos::TSP_Algo_Genetic(t_graph);
+    }
+
     do {
-        cout << "Select options:\n"
+        if (is_threaded) {
+            cout << "\n--------------------------------------------------------------------------------\n"
+                 << "Threaded Genetic Algorithm\n"
+                 << "Number of threads: " << algo_tg->getThreadCount() << "\n"
+                 << "Best fitness so far: " << algo_tg->getCurrentFitness() << "\n";
+        } else {
+            cout << "\n--------------------------------------------------------------------------------\n"
+                 << "Genetic Algorithm\n"
+                 << "Best fitness so far: " << algo_g->getCurrentFitness() << "\n";
+        }
+        cout << "Current generation: " << current_gen << "\n"
+             << "Population size: " << pop_size << "\n\n"
+             << "Select options:\n"
              << "0 - Exit\n"
-             << "1 - Run algorithm\n"
+             << "1 - Run algorithm for " << num_gens << " generations\n"
              << "2 - Next generation\n"
              << "3 - Change population size\n"
              << "4 - Change number of generations\n";
@@ -122,20 +134,44 @@ void testGenetic(Graph* t_graph) {
             case 0:
                 break;
             case 1:
-                algo_g.run(num_runs);
+                if (is_threaded) {
+                    algo_tg->run(num_gens);
+                } else {
+                    algo_g->run(num_gens);
+                }
+                current_gen += num_gens;
+                break;
             case 2:
-                algo_g.tick();
+                if (is_threaded) {
+                    algo_tg->tick();
+                } else {
+                    algo_g->tick();
+                }
+                current_gen++;
+                break;
             case 3:
                 cout << "Enter size:\n";
                 cin >> pop_size;
-                algo_g.changePopulationSize(pop_size);
+                if (is_threaded) {
+                    algo_tg->changePopulationSize(pop_size);
+                } else {
+                    algo_g->changePopulationSize(pop_size);
+                }
+                break;
             case 4:
                 cout << "Enter number of gens:\n";
-                cin >> num_runs;
+                cin >> num_gens;
+                break;
             default:
                 cout << "Invalid choice!\n";
             }
-    } while (choice != -1);
+    } while (choice != 0);
+
+    if (is_threaded) {
+        delete algo_tg;
+    } else {
+        delete algo_g;
+    }
 }
 
 int displayGUI(int argc, char *argv[]) {
@@ -178,9 +214,9 @@ void printLicense() {
     "*                                                                              *\n"
     "*                                 GraphPlot                                    *\n"
     "*  Author: Lucas Frey                                                          *\n"
-    "*  Version: 1.1                                                                *\n"
-    "*  Date Last Modified: 8/26/2017                                               *\n"
+    "*  Version: 2.0                                                                *\n"
+    "*  Date Last Modified: 10/16/2017                                              *\n"
     "*                                                                              *\n"
-    " ******************************************************************************\n\n\n";
+    " ******************************************************************************\n\n";
 }
 
